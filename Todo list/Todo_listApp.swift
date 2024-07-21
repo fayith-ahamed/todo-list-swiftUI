68a2557826eef3268655b8c237b7836a12326b01
fayith-ahamed <fayithahamed007@gmail.com>
2024-07-21 12:18:29 +0530

    Initial Commit

---
+++ Todo list/Todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_listApp.swift
+//  Todo list
+//
+//  Created by Fayith  on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
